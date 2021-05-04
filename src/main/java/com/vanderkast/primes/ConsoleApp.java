65106449642aee0e240242d2162a4aa1c7965bfb
Vanderkast <vanderkast@yandex.ru>
2021-05-04 18:51:34 +0300

    update

--- src/main/java/com/vanderkast/primes/ConsoleApp.java
+++ src/main/java/com/vanderkast/primes/ConsoleApp.java
@@ -4,14 +4,18 @@ import com.vanderkast.primes.domain.BigIntegerPrimeApprove;
 import com.vanderkast.primes.domain.LongPrimeApprove;
 import com.vanderkast.primes.domain.PrimeApproveFacade;
 
-public class StartPoint {
+public class ConsoleApp {
     public static void main(String[] args) {
+        if(args == null || args.length == 0) {
+            System.out.println("No numbers passed");
+            return;
+        }
         var facade = new PrimeApproveFacade(new LongPrimeApprove(), new BigIntegerPrimeApprove());
         String prime;
         for (String arg : args) {
             try {
-                prime = facade.isPrime(arg)? "" : "not";
-                System.out.printf("Number %s is %s prime%n", arg, prime);
+                prime = facade.isPrime(arg) ? "prime" : "not prime";
+                System.out.printf("Number %s is %s%n", arg, prime);
             } catch (NumberFormatException e) {
                 System.out.println("Incorrect number " + arg);
             }
